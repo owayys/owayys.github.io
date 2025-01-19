5fcadb6c279e5d79ecd11fc9aa0c33c9328b0568
Owais Ahsan <owais.ahsan@hotmail.com>
2025-01-19 20:24:16 +0500

    Style changes

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -24,15 +24,6 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Darkmode(),
     Component.DesktopOnly(
       Component.RecentNotes({
-        title: "Recent Writing",
-        limit: 4,
-        filter: (f) =>
-          f.slug!.startsWith("posts/") && f.slug! !== "posts/index" && !f.frontmatter?.noindex,
-        linkToMore: "posts/" as SimpleSlug,
-      }),
-    ),
-    Component.DesktopOnly(
-      Component.RecentNotes({
         title: "Recent Notes",
         limit: 2,
         filter: (f) => f.slug!.startsWith("thoughts/"),
