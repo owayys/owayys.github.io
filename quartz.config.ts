e28f0a34a0aa90ee23e2a4a36e4b7cad6c743cdb
owayys <owais.ahsan@hotmail.com>
2025-05-17 01:33:44 +0500

    update theme

--- quartz.config.ts
+++ quartz.config.ts
@@ -29,28 +29,52 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#fbf1c7",
-          lightgray: "#ebdbb2",
-          gray: "#b57614",
-          darkgray: "#3c3836",
-          dark: "#427b58",
-          secondary: "#282828",
-          tertiary: "#b57614",
-          highlight: "rgba(189, 174, 147, 0.15)",
-          textHighlight: "#282828",
+          light: "#faf8f8",
+          lightgray: "#e5e5e5",
+          gray: "#b8b8b8",
+          darkgray: "#4e4e4e",
+          dark: "#2b2b2b",
+          secondary: "#284b63",
+          tertiary: "#84a59d",
+          highlight: "rgba(143, 159, 169, 0.15)",
+          textHighlight: "#fff23688",
         },
         darkMode: {
-          light: "#1d2021", // background
-          lightgray: "#3c3836", // outlines + search
-          gray: "#fabd2f", // date, x min read under header
-          darkgray: "#ebdbb2", // dec date
-          dark: "#8ec07c", // section
-          secondary: "#fbf1c7", // header, created with etc
-          tertiary: "#fabd2f", // link hover + tag desc
-          highlight: "rgba(146, 131, 116, 0.15)",
-          textHighlight: "#fbf1c7",
+          light: "#161618",
+          lightgray: "#393639",
+          gray: "#646464",
+          darkgray: "#d4d4d4",
+          dark: "#ebebec",
+          secondary: "#7b97aa",
+          tertiary: "#84a59d",
+          highlight: "rgba(143, 159, 169, 0.15)",
+          textHighlight: "#b3aa0288",
         },
-      },
+      }
+      // colors: {
+      //   lightMode: {
+      //     light: "#fbf1c7",
+      //     lightgray: "#ebdbb2",
+      //     gray: "#b57614",
+      //     darkgray: "#3c3836",
+      //     dark: "#427b58",
+      //     secondary: "#282828",
+      //     tertiary: "#b57614",
+      //     highlight: "rgba(189, 174, 147, 0.15)",
+      //     textHighlight: "#282828",
+      //   },
+      //   darkMode: {
+      //     light: "#1d2021", // background
+      //     lightgray: "#3c3836", // outlines + search
+      //     gray: "#fabd2f", // date, x min read under header
+      //     darkgray: "#ebdbb2", // dec date
+      //     dark: "#8ec07c", // section
+      //     secondary: "#fbf1c7", // header, created with etc
+      //     tertiary: "#fabd2f", // link hover + tag desc
+      //     highlight: "rgba(146, 131, 116, 0.15)",
+      //     textHighlight: "#fbf1c7",
+      //   },
+      // },
     },
   },
   plugins: {
