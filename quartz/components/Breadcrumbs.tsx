ae7119c3dd5f2147d0388bb25babea11cd2a0cbb
owaisatcarbonteq <owais.ahsan@carbonteq.com>
2025-08-05 16:23:41 +0500

    update breadcrumb opts

--- quartz/components/Breadcrumbs.tsx
+++ quartz/components/Breadcrumbs.tsx
@@ -29,7 +29,7 @@ interface BreadcrumbOptions {
 }
 
 const defaultOptions: BreadcrumbOptions = {
-  spacerSymbol: ">",
+  spacerSymbol: "›",
   rootName: "root",
   resolveFrontmatterTitle: true,
   showCurrentPage: true,
