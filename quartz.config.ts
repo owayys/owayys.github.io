d9bd14e231c06fa94d2486d2f91812b8e7f025e4
owayys <owais.ahsan@hotmail.com>
2025-09-21 22:53:41 +0500

    update body font

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "JetBrains Mono",
-        body: "Mona Sans",
+        body: "Atkinson Hyperlegible",
         code: "JetBrains Mono",
       },
       colors: {
