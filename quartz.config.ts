7a04c5ddd43b464b2a9b2a5def603abecf82cd40
owayys <owais.ahsan@hotmail.com>
2025-09-21 23:17:53 +0500

    add goatcounter analytics

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,8 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "goatcounter",
+      websiteId: "owayys",
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
