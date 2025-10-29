36ebefbff1907f4f5a2f641ff4cfb8a5f11ec99c
owayys <owais.ahsan@hotmail.com>
2025-10-29 21:33:39 +0500

    update constants component

--- quartz/components/Constants.tsx
+++ quartz/components/Constants.tsx
@@ -3,20 +3,17 @@ import { resolveRelative } from "../util/path"
 import { QuartzPluginData } from "../plugins/vfile"
 import { byDateAndAlphabetical } from "./PageList"
 import style from "./styles/recentNotes.scss"
-import { Date, getDate } from "./Date"
 import { GlobalConfiguration } from "../cfg"
 import { i18n } from "../i18n"
 import { classNames } from "../util/lang"
 
 interface Options {
   title?: string
-  filter: (f: QuartzPluginData) => boolean
   sort: (f1: QuartzPluginData, f2: QuartzPluginData) => number
 }
 
 const defaultOptions = (cfg: GlobalConfiguration): Options => ({
-  title: "Constants",
-  filter: ({ slug }) => slug?.includes("/") === false && slug?.includes("index") === false,
+  title: "Constant",
   sort: byDateAndAlphabetical(cfg),
 })
 
@@ -28,7 +25,9 @@ export default ((userOpts?: Partial<Options>) => {
     cfg,
   }: QuartzComponentProps) => {
     const opts = { ...defaultOptions(cfg), ...userOpts }
-    const pages = allFiles.filter(opts.filter).sort(opts.sort)
+    const pages = allFiles
+      .filter(({ slug }) => !slug?.includes("/") && !slug?.includes("index"))
+      .sort(opts.sort)
     return (
       <div class={classNames(displayClass, "recent-notes")}>
         {opts.title && <h3>{opts.title}</h3>}
